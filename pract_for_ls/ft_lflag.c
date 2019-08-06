9306658860be4769bb3bc6b68a2f316bafbe64f9
Nqabutho LUNGA <nlunga@c5r7s7.wethinkcode.co.za>
2019-08-06 16:43:07 +0200

    been doing exams all day

--- pract_for_ls/ft_lflag.c
+++ pract_for_ls/ft_lflag.c
@@ -6,7 +6,7 @@
 /*   By: nlunga <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 20:14:28 by nlunga            #+#    #+#             */
-/*   Updated: 2019/08/05 16:56:13 by nlunga           ###   ########.fr       */
+/*   Updated: 2019/08/06 16:30:12 by nlunga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,14 @@ int	ft_lflag(int argc,char **argv, t_flags *m_flags)
 	if (m_flags->l_flag == 1)
 	{
 		struct stat fileinfo;
+		struct passwd *pwd;
+		struct group *gwd;
+
+		//pwd = NULL;
 		if (stat(argv[i], &fileinfo) < 0)
-			return (1);		
+			return (1);
 		printf( (S_ISDIR(fileinfo.st_mode)) ? "d" : "-");
-		printf( (fileinfo.st_mode & S_IRUSR) ? "r" : " -");
+		printf( (fileinfo.st_mode & S_IRUSR) ? "r" : "-");
 		printf( (fileinfo.st_mode & S_IWUSR) ? "w" : "-");
 		printf( (fileinfo.st_mode & S_IXUSR) ? "x" : "-");
 		printf( (fileinfo.st_mode & S_IRGRP) ? "r" : "-");
@@ -48,6 +52,12 @@ int	ft_lflag(int argc,char **argv, t_flags *m_flags)
 		printf( (fileinfo.st_mode & S_IXOTH) ? "x" : "-");
 		printf(" ");
 		printf("%d ",fileinfo.st_nlink);
+		if ((pwd = getpwuid(fileinfo.st_uid)))
+			printf("%s ",pwd->pw_name);
+		if ((gwd = getgrgid(fileinfo.st_gid)))
+			printf("%s ",gwd->gr_name);
+	//	printf("%u ",fileinfo.st_uid);
+	//	printf("%u ",fileinfo.st_gid);
 		printf(" %lld ",fileinfo.st_size);
 		ft_m_time(argv[i]);
 		printf("%s\n", argv[i]);
