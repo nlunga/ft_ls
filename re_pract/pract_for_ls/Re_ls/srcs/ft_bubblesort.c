65dc9a5cdb464b1243f8f6b355fd453ad88d0050
Innocent <nlunga@student.wethinkcode.co.za>
2019-10-02 18:20:29 +0200

    get time to work

--- re_pract/pract_for_ls/Re_ls/srcs/ft_bubblesort.c
+++ re_pract/pract_for_ls/Re_ls/srcs/ft_bubblesort.c
@@ -6,7 +6,7 @@
 /*   By: nlunga <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/06 10:43:17 by nlunga            #+#    #+#             */
-/*   Updated: 2019/10/02 14:52:23 by nlunga           ###   ########.fr       */
+/*   Updated: 2019/10/02 18:19:29 by innocent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,70 @@ void	ft_bubblesort(t_dir order[])
 			i++;
 		}
 	}
+}
+
+int		sort_time(char *file, char *file2)
+{
+	struct stat		buff;
+	struct stat		buff2;
+
+	stat(file, &buff);
+	stat(file2, &buff2);
+	return (buff.st_mtime < buff2.st_mtime);
+}
+
+void	ft_swap_str(char **str, char **str1)
+{
+	char	*temp;
+
+	temp = *str;
+	*str = *str1;
+	*str1 = temp;
+}
+
+void	ft_store_time(char **path, char **path1, char *str)
+{
+	struct stat		buf;
+	struct stat		buf1;
+	//struct timespec st_mtim;
+	char			*s1;
+	char			*s2;
+
+	s1 = ft_strjoin(str, *path);
+	s2 = ft_strjoin(str, *path1);
+	stat(s1, &buf);
+	stat(s2, &buf1);
+	if (buf.st_mtime < buf1.st_mtime)
+		ft_swap_str(path, path1);
+	else
+	{
+		if (buf.st_mtime == buf1.st_mtime)
+		{
+//			if (buf.st_mtimespec.tv_nsec < buf1.st_mtimespec.tv_nsec)
+			if (buf.st_mtim.tv_sec < buf1.st_mtim.tv_sec)
+				ft_swap_str(path, path1);
+		}
+	}
+}
+
+char	**ft_tflag(char *dir, char **array, int n)
+{
+	int		i;
+	int		k;
+	char	*str;
+
+	str = ft_strjoin(dir, "/");
+	i = 0;
+	while (i < n - 1)
+	{
+		k = 0;
+		while (k < (n - i - 1))
+		{
+			ft_store_time(&array[k], &array[k + 1], str);
+			k++;
+		}
+		i++;
+	}
+	free(str);
+	return (array);
 }
--- re_pract/pract_for_ls/Re_ls/srcs/ft_ls.c
+++ re_pract/pract_for_ls/Re_ls/srcs/ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: nlunga <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 09:23:53 by nlunga            #+#    #+#             */
-/*   Updated: 2019/10/02 14:52:19 by nlunga           ###   ########.fr       */
+/*   Updated: 2019/10/02 17:38:06 by innocent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 int	main(int ac, char **av)
 {
-	int		i;
 	int		size = 1024;
 	t_dir	start[size];
 	t_flags	*mflag;
 
 	if (!(mflag = (t_flags *)malloc(sizeof(t_flags))))
 		return (0);
-	i = 0;
 	if (ac == 1 && av)
 	{
 		ft_opendir(".", start);
