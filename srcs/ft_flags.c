c9c2a60a1c9746fd700b62d59c733011e0c5b9e8
Nqabutho LUNGA <nlunga@student.wethinkcode.co.za>
2020-02-03 15:48:10 +0200

    Check why my j isn't incrementing on the ft_flags function

--- srcs/ft_flags.c
+++ srcs/ft_flags.c
@@ -6,7 +6,7 @@
 /*   By: nlunga <nlunga@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 11:59:52 by nlunga            #+#    #+#             */
-/*   Updated: 2020/02/03 15:15:39 by nlunga           ###   ########.fr       */
+/*   Updated: 2020/02/03 15:45:23 by nlunga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,23 +30,27 @@ void	ft_lflag(char **path, t_flags *mflag, t_dir *data)
 	if (mflag->l_flag == 1)
 	{
 		printf(">>>>> %s\n <<<<<", path[i]);
-		j = 0;
+		// j = 0; This is the original
 		while (path[i] != NULL)
 		{
+			j = 0;
 			//data[j]->pathft_addpath(, path[i]);
-			k = 0;
+			// k = 0; This is The original
 			printf("This  is path %s ", path[i]);
 			if (ft_isdir(path[i]) && (mflag->a_flag == 0))
 			{
+				k = 0;
 				ft_opendir(path[i], data);
 				data[j].path = ft_addpath(path[i], data[k].name);
 				while (data[k].name != NULL)
 				{
-					
+					printf(">>>>>> This is the name %s", data[k].name);
 					printf("\n%d\n", k);
-					if (lstat(data[j].path, &buf) < 0)
+					// if (lstat(data[j].path, &buf) < 0) // Original I might wanna check it for debugging
+					if (lstat(data[j].name, &buf) < 0)
 						ft_putendl("lstat: FAILURE");
 				//		return (1);
+					printf("\nThis is j: %d\n", j);
 					ft_perm(buf);
 					ft_getid(buf);
 					// printf("%s ", data[j].path);
